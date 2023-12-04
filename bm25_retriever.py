3092fe1444dfc9ddbb77972ddbcfecd036a7ee66
ehsu4220 <31673535+ehsu4220@users.noreply.github.com>
2023-12-04 01:47:52 -0500

    Scripts
    Chunk commit of scripts responsible for preprocessing and clustering

---
+++ bm25_retriever.py
@@ -0,0 +1,123 @@
+from rank_bm25 import BM25Okapi
+
+import json
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from collections import defaultdict
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# NLTK setup
+nltk.download('punkt')
+nltk.download('stopwords')
+stemmer = PorterStemmer()
+stop_words = set(stopwords.words('english'))
+
+# Helper function that handled normalization, stemming, and stopwords
+def preprocess_text(text):
+    tokens = word_tokenize(text.lower())  # Convert to lowercase and tokenize
+    filtered_tokens = [stemmer.stem(word) for word in tokens if word.isalnum() and word not in stop_words]
+    return ' '.join(filtered_tokens)
+
+def preprocess_user_data(document_list):
+    for doc in document_list:
+        doc['text'] = preprocess_text(doc['text'])
+        doc['title'] = preprocess_text(doc['title'])
+        # print(f"{doc['text']} : {doc['title']}")
+
+# Process the questions and extract the user profiles, associated inputs, and IDs
+def process_questions(path, target_path):
+    with open(path, 'r') as file:
+        data = json.load(file)
+
+    # Process the json
+    processed_data = {item['id']: [item['input'], item['profile']] for item in data}
+    
+    # Normalize input and user profile
+    for id in processed_data.keys():
+        processed_data[id][0] = preprocess_text(processed_data[id][0].split("article: ", 1)[1])
+        #print(f"Input prompt: {processed_data[id][0]}")
+        preprocess_user_data(processed_data[id][1])
+
+    # Save into file
+    with open(target_path, 'w') as json_file:
+        json.dump(processed_data, json_file)
+        
+    
+    return processed_data
+
+
+# Process the outputs and formulate the IDs and associated outputs
+def process_outputs(path):
+    with open(path, 'r') as file:
+        data = json.load(file)
+        
+    # Process the json
+    outputs = data['golds']
+    processed_data = {item['id']: item['output'] for item in outputs}
+    
+    # Normalize output values
+    
+    return processed_data
+
+
+# BM25 retriever model
+def retrieve_relevant_k_docs(input, user_profile, k=20, minimum_profile_size=100):
+
+    if k > minimum_profile_size or len(user_profile) < minimum_profile_size:
+        return -1
+
+    # Tokenize the list of documents
+    documents = [f"{item['title']} {item['text']}" for item in user_profile]
+    tokenized_documents = [doc.split(" ") for doc in documents]
+    
+    # Generate reverse lookup dictionary for converting document back to ID
+    reverse_document_lookup = dict()
+    for item in user_profile:
+        reverse_document_lookup[f"{item['title']} {item['text']}"] = (item['id'], item['category'])
+    
+    # Tokenize the input query
+    tokenized_input = input.split(" ")
+    
+    # Retriever
+    bm25 = BM25Okapi(tokenized_documents)
+    
+    # retrieve documents
+    retrieved_documents = bm25.get_top_n(tokenized_input, documents, k)
+    
+    # return the IDs associated with top k
+    return [reverse_document_lookup[key][0] for key in retrieved_documents if key in reverse_document_lookup]
+    
+    
+
+
+def main():
+    
+    # Processes the questions: stopwords, normalization, stemming
+    ## Results are saved into json files in processed_data/ in order to avoid the need to constantly process this.
+    #process_questions("data/lamp2/train/questions.json", "processed_data/lamp2/train/questions.json")
+    #process_questions("data/lamp4/train/questions.json", "processed_data/lamp4/train/questions.json")
+    process_questions("data/lamp2/validate/questions.json", "processed_data/lamp2/validate/questions.json")
+    process_questions("data/lamp4/validate/questions.json", "processed_data/lamp4/validate/questions.json")
+
+    # # Outputs
+    # lamp2_outputs = process_outputs("data/lamp2/train/outputs.json")
+    # lamp4_outputs = process_outputs("data/lamp4/train/outputs.json")
+    
+    # load the preprocessed user profiles
+    # with open("processed_data/lamp2/train/questions.json", 'r') as lamp2_train:
+    #     data = json.load(lamp2_train)
+    
+    # for id in data:
+        # retrieved_documents_ids = retrieve_relevant_k_docs(data[id][0], data[id][1])
+        
+        # Retrieve the text associated with the profile and save somewhere for later fine-tuning on the LLM
+    
+    # Form (input, output) pairs.
+    print("debug")
+
+if __name__ == '__main__':
+    main()
---
+++ diversify_corpus.py
@@ -0,0 +1,295 @@
+import json
+import os
+import random
+import re
+import string
+from prettytable import PrettyTable
+
+import nltk
+import numpy as np
+import pandas as pd
+
+from gensim.models import Word2Vec
+import gensim.downloader as api
+
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
+
+# Load pre-trained word2vec model
+#wv = api.load('word2vec-google-news-300')
+
+SEED = 42
+random.seed(SEED)
+os.environ["PYTHONHASHSEED"] = str(SEED)
+np.random.seed(SEED)
+
+### Operation settings
+# 1: vectors
+# 2: number of clusters
+# 3: Mini-batch size
+which_hyperparam = 2
+which_task = 4
+
+lamp2_vector_sizes = [140]
+lamp2_cluster_numbers = [14]
+lamp2_cluster_sizes = [32]
+
+lamp4_vector_sizes = [220]
+lamp4_cluster_numbers = [14]
+
+# For Lamp2
+vector_sizes = [220] # Settled on 140, maximized inertia
+cluster_numbers = list(range(2, 36))
+cluster_sizes = [32] # mb
+
+
+# Tables from hyperparameter tuning
+model_vector_tuning_headers = ['# of Model Vectors', 'Inertia', 'Silhouette coefficient']
+num_clusters_headers = ['# of Clusters', 'Inertia', 'Silhouette coefficient']
+mini_batch_headers = ['Mini-batch Size', 'Inertia', 'Silhouette coefficient']
+
+# Find more stable metrics of the number of clusters
+if which_hyperparam == 1:
+    average_inertias = {k : [] for k in vector_sizes}
+    average_silhouette = {k : [] for k in vector_sizes}
+elif which_hyperparam == 2:
+    average_inertias = {k : [] for k in cluster_numbers}
+    average_silhouette = {k : [] for k in cluster_numbers}
+elif which_hyperparam == 3:
+    average_inertias = {k : [] for k in cluster_sizes}
+    average_silhouette = {k : [] for k in cluster_sizes}
+
+def generate_models(path, task):
+    ## load the pre-processed documents 
+    with open(path, 'r') as file:
+        training_data = json.load(file)
+        
+    # Train model using list of all of the documents
+    tokenized_all_docs = []
+    for id in training_data:
+        user_profile = training_data[id][1]
+        documents = [f"{item['title']} | {item['text']}" for item in user_profile]
+        tokenized_documents = [doc.split(" ") for doc in documents]
+        tokenized_all_docs.extend(tokenized_documents)
+        
+    print(f"All documents in training data tokenized: {len(tokenized_all_docs)}")
+    for size_of_vector in vector_sizes:
+        print(f"begining training of Word2Vec model of size {size_of_vector}")
+        model = Word2Vec(sentences=tokenized_all_docs, vector_size=size_of_vector, workers=2, seed=SEED)
+        model.save(f"models/lamp{task}/{size_of_vector}.bin")
+
+
+# list_of_docs: list of tokenized documents
+def vectorize(list_of_docs, model):
+    """Generate vectors for list of documents using a Word Embedding
+
+    Args:
+        list_of_docs: List of documents
+        model: Gensim's Word Embedding
+
+    Returns:
+        List of document vectors
+    """
+    features = []
+
+    for tokens in list_of_docs:
+        zero_vector = np.zeros(model.vector_size)
+        vectors = []
+        for token in tokens:
+            if token in model.wv:
+                try:
+                    vectors.append(model.wv[token])
+                except KeyError:
+                    continue
+        if vectors:
+            vectors = np.asarray(vectors)
+            avg_vec = vectors.mean(axis=0)
+            features.append(avg_vec)
+        else:
+            features.append(zero_vector)
+    return features
+
+
+# Returns a dictionary containing the vectorized documents for each user profile for the associated ID
+def word_2_vec(path, model_path, minimum_profile_size=0):
+
+    # load model from bin
+    model = Word2Vec.load(model_path)
+    
+    # Load the data that you want vectorized
+    with open(path, 'r') as file:
+        data = json.load(file)
+    
+    vectorized_documents_id = dict()
+    
+    total_user_profiles = len(data)
+    current = 1
+    
+    docs = dict()
+    tokenized_docs = dict()
+    
+    for id in data:
+        # print(f"{current}/{total_user_profiles}")
+        current += 1
+        user_profile = data[id][1]
+        
+        # Skip user profiles with not enough documents to cluster        
+        if len(user_profile) < minimum_profile_size:
+            continue
+
+        # Tokenize the list of documents
+        documents = [f"{item['title']} {item['text']}" for item in user_profile]
+        tokenized_documents = [doc.split(" ") for doc in documents]
+        
+        vectorized_docs = vectorize(tokenized_documents, model=model)
+        
+        vectorized_documents_id[id] = vectorized_docs
+        docs[id] = documents
+        tokenized_docs[id] = tokenized_documents
+        
+    return vectorized_documents_id, tokenized_docs, docs
+
+def mbkmeans_clusters(
+	X, 
+    k, 
+    mb, 
+    print_silhouette_values,
+    # For hyperparameter testing
+    hyperparameter_tested,
+    model_vector_size
+):
+    """Generate clusters and print Silhouette metrics using MBKmeans
+
+    Args:
+        X: Matrix of features.
+        k: Number of clusters.
+        mb: Size of mini-batches.
+        print_silhouette_values: Print silhouette values per cluster.
+
+    Returns:
+        Trained clustering model and labels based on X.
+    """
+    km = MiniBatchKMeans(n_clusters=k, batch_size=mb).fit(X)
+    # print(f"For n_clusters = {k}")
+    # print(f"Silhouette coefficient: {silhouette_score(X, km.labels_):0.2f}")
+    # print(f"Inertia:{km.inertia_}")
+
+    #### Add metrics to tested hyperparameters ####
+    if hyperparameter_tested == 1: # Model Vectors
+        average_inertias[model_vector_size].append(km.inertia_)
+        average_silhouette[model_vector_size].append(float(f"{silhouette_score(X, km.labels_):0.2f}"))
+    elif hyperparameter_tested == 2: # number of clusters
+        average_inertias[k].append(km.inertia_)
+        average_silhouette[k].append(float(f"{silhouette_score(X, km.labels_):0.2f}"))
+    elif hyperparameter_tested == 3: # mini-batch size
+        average_inertias[mb].append(km.inertia_)
+        average_silhouette[mb].append(float(f"{silhouette_score(X, km.labels_):0.2f}"))
+    else:
+        pass
+
+    if print_silhouette_values:
+        sample_silhouette_values = silhouette_samples(X, km.labels_)
+        print(f"Silhouette values:")
+        silhouette_values = []
+        for i in range(k):
+            cluster_silhouette_values = sample_silhouette_values[km.labels_ == i]
+            silhouette_values.append(
+                (
+                    i,
+                    cluster_silhouette_values.shape[0],
+                    cluster_silhouette_values.mean(),
+                    cluster_silhouette_values.min(),
+                    cluster_silhouette_values.max(),
+                )
+            )
+        silhouette_values = sorted(
+            silhouette_values, key=lambda tup: tup[2], reverse=True
+        )
+        for s in silhouette_values:
+            print(
+                f"    Cluster {s[0]}: Size:{s[1]} | Avg:{s[2]:.2f} | Min:{s[3]:.2f} | Max: {s[4]:.2f}"
+            )
+    return km, km.labels_
+
+
+def main():
+    
+    # Test performance of the different models with different amounts of vectors
+    for size_of_vector in vector_sizes: # settled on 80 for vector size
+    
+        print(f"Vector Size: {size_of_vector}")
+    
+        # Find the document vectors for the preprocessed dataset
+        user_document_vectors, tokenized_docs, docs = word_2_vec(f"processed_data/lamp{which_task}/validate/questions.json", f"models/lamp{which_task}/{size_of_vector}.bin", minimum_profile_size=100)
+        
+        which_id = 1
+        # Form the clusters based on the document vectors
+        for id in user_document_vectors.keys():
+            print(f"{which_id}/{len(user_document_vectors)}")
+            which_id += 1
+            
+            for cluster_size in cluster_sizes:
+                
+                for cluster_num in cluster_numbers:
+                    #print(f"# of clusters: {cluster_num}")
+                    clustering, cluster_labels = mbkmeans_clusters(
+                    X=user_document_vectors[id],
+                    k=cluster_num,
+                    mb=cluster_size,
+                    print_silhouette_values=False,
+                    hyperparameter_tested=which_hyperparam, # Hyperparameter testing
+                    model_vector_size=size_of_vector
+                    )
+                    df_clusters = pd.DataFrame({
+                        "text": docs[id],
+                        "tokens": [" ".join(text) for text in tokenized_docs[id]],
+                        "cluster": cluster_labels
+                    })
+        print("--------------------------------------------------------------------------------------------------------------------------")
+
+    # Save the results of hyperparameter tuning
+    ## Vector size
+    if which_hyperparam == 1:
+        vector_size_table = PrettyTable()
+        vector_size_table.field_names = model_vector_tuning_headers
+        for key in sorted(average_inertias.keys()):
+            row = [key, 
+                sum(average_inertias[key]) / len(average_inertias[key]), 
+                sum(average_silhouette[key]) / len(average_silhouette[key])]
+            vector_size_table.add_row(row)
+        vector_size_table_string = vector_size_table.get_string()
+        with open(f"tuning/lamp{which_task}/Model_vector_tuning.txt", 'w') as file:
+            file.write(vector_size_table_string)
+    
+    # ## Number of clusters
+    elif which_hyperparam == 2:
+        num_clusters_table = PrettyTable()
+        num_clusters_table.field_names = num_clusters_headers
+        # Average metrics
+        for key in sorted(average_inertias.keys()):
+            row = [key, 
+                sum(average_inertias[key]) / len(average_inertias[key]), 
+                sum(average_silhouette[key]) / len(average_silhouette[key])]
+            num_clusters_table.add_row(row) 
+        num_clusters_table_string = num_clusters_table.get_string()
+        with open(f"tuning/lamp{which_task}/num_clusters_tuning.txt", 'w') as file:
+            file.write(num_clusters_table_string)
+    
+    # ## Mini batch size
+    elif which_hyperparam == 1:
+        mini_batch_table = PrettyTable()
+        mini_batch_table.field_names = mini_batch_headers
+        for key in sorted(average_inertias.keys()):
+            row = [key, 
+                sum(average_inertias[key]) / len(average_inertias[key]), 
+                sum(average_silhouette[key]) / len(average_silhouette[key])]
+            mini_batch_table.add_row(row)
+        mini_batch_table_string = mini_batch_table.get_string()
+        with open(f"tuning/{which_task}/mini_batch_tuning.txt", 'w') as file:
+            file.write(mini_batch_table_string)
+
+if __name__ == '__main__':
+    main()
